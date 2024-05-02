c4c6eaf8d9fe80dfe3544efc7f639f07a14ea6e8
Andrey Onkhonov <148449110+torokar@users.noreply.github.com>
2024-05-02 19:39:08 +0800

    Add files via upload
    Добавлен функция расчета времени time_star - инициализирует время с начала эпохи дальше total_time подсчитывает время разница между текущим и начала получаяя в секундах users_time выводит время после нажатия на кнопку Enter.
    добавлена функция расчета символов в минуту  - time_star записывает текущие время, после нажатия на кнопку Enter программа снова вызывает текущие время получая разницу во времени расчет происходит в переменной typing_speed  = введенные символы пользователям / на разницу во времени. И при помощи characters_minute выводит результат 
    Дальнейшие действия:
    1. Создать приветственное окно с кнопкой "Начать"
    2. Изменить текстовые файлы 
    3. Создать кнопку Reset или же Начать заново
    4. Создать еще одну функцию которая будет расчитывать количество процентов точности написани(Самая проблемная)

--- SpeedTypingTest/speedtyping/main.py
+++ SpeedTypingTest/speedtyping/main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import time
 
 window = pygame.display.set_mode([750, 500])
 
@@ -9,12 +10,13 @@ def main():
     icon = pygame.image.load("image/icon1.png")
     pygame.display.set_icon(icon)
     background = pygame.image.load("image/background.jpg")
+    pygame.display.set_caption("Скорость печати")
     font = pygame.font.Font(None, 60)
     Input_font = pygame.font.Font(None, 36)
     clock = pygame.time.Clock()
     user_text = ""
 
-    # Список текстов
+
     text_files = ["text1.txt", "text2.txt", "text3.txt", "text4.txt"]
     # Выбор случайного файла из списка
     selected_file = random.choice(text_files)
@@ -23,18 +25,25 @@ def main():
     with open(selected_file, "r", encoding='utf-8') as file:
         file_text = file.read()
 
-    text_surf = font.render("Speed Typing Test", True, "Yellow")  # Рендеринг надписи "Speed Typing Test"
-    file_surf = Input_font.render(file_text, True, (255, 255, 255))  # Рендеринг текста из файла
+    text_ = font.render("Тест на скорость печати", True, "Yellow")
+    file_surf = Input_font.render(file_text, True, ('White'))
 
-    input_rect = pygame.Rect(10, 200, 140, 32)
-    color_active = pygame.Color('lightskyblue3')
-    color_passive = pygame.Color(255, 255, 255)
+    input_rect = pygame.Rect(50, 200, 140, 32)
+    color_active = pygame.Color('Yellow')
+    color_passive = pygame.Color('White')
     color = color_passive
     active = False
+    time_start = None
+    users_time = None
+    symbols_enter = 0
+    #начала подсчета времени
+    time_start = time.time()
 
     while True:
         window.blit(background, (0, 0))
-        window.blit(file_surf, (50, 100))  # Отображение текста из файла
+        window.blit(file_surf, (50, 100))
+
+        window.blit(text_, (150, 10))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -45,13 +54,28 @@ def main():
                     active = True
                 else:
                     active = False
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if active == True:
                     if event.key == pygame.K_BACKSPACE:
                         user_text = user_text[:-1]
-                    else:
-                        user_text += event.unicode
+                        symbols_enter -= 1
+                    elif event.key == pygame.K_RETURN:
+                        if time_start is not None:
+                            #расчет времени
+                            total_time = time.time() - time_start
+                            
 
+                            typing_speed = symbols_enter / total_time
+
+                            time_start = None
+                            users_time = Input_font.render(f"Время: {round(total_time, 2)} секунд", True, (255, 255, 255))
+
+                            characters_minute = Input_font.render(f"Скорость печати: {round(typing_speed, 2)} знаков в минуту", True, pygame.Color('White'))
+
+                    else:
+
+                        user_text += event.unicode
+                        symbols_enter += 1
         if active:
             color = color_active
         else:
@@ -60,12 +84,21 @@ def main():
         pygame.draw.rect(window, color, input_rect, 2)
         text_surf = Input_font.render(user_text, True, (255, 255, 255))
         window.blit(text_surf, (input_rect.x + 5, input_rect.y + 5))
+        pygame.display.set_caption("Скорость печати")
+
 
         input_rect.w = max(10, text_surf.get_width() + 10)
+
+        if users_time is not None:
+            window.blit(users_time, (10, 300))
+            window.blit(characters_minute, (10, 350))
+
 
         pygame.display.flip()
         clock.tick(60)
 
+
 if __name__ == '__main__':
     main()
     pygame.quit()
+
