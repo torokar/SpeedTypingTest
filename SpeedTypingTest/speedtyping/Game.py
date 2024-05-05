0b6f79a173ed61871a9a16e946b15b315f58cab1
Andrey Onkhonov <148449110+torokar@users.noreply.github.com>
2024-05-05 15:55:06 +0800

    Add files via upload
    Разделил функцию main на еще на 3 функции. Создал стартовое окно для запуска теста

---
+++ SpeedTypingTest/speedtyping/Game.py
@@ -0,0 +1,132 @@
+import pygame
+import sys
+from main import Button
+import random
+import time
+
+
+WIDTH, HEIGHT = 750, 500
+FRAME_RATE = 60
+pygame.init()
+
+
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("")
+
+
+start_button = Button(WIDTH/2-(250/2), 200, 300, 100, "",
+                      "image/green_button.png","image/green_button_hover.png", "button.mp3")
+
+def load_image():
+    icon = pygame.image.load("image/icon1.png")
+    pygame.display.set_icon(icon)
+    background = pygame.image.load("image/background.jpg")
+    return background
+
+def setup_users_inter(font, input_font):
+    text_ = font.render("Тест на скорость печати", True, "Yellow")
+    input_rect = pygame.Rect(50, 200, 140, 32)
+    color_active = pygame.Color('Yellow')
+    color_passive = pygame.Color('White')
+    return text_, input_rect, color_active, color_passive
+
+def start_test():
+    background = load_image()
+    font = pygame.font.Font(None, 60)
+    input_font = pygame.font.Font(None, 36)
+    clock = pygame.time.Clock()
+    user_text = ""
+
+    text_files = ["text1.txt", "text2.txt", "text3.txt", "text4.txt"]
+    selected_file = random.choice(text_files)
+    with open(selected_file, "r", encoding='utf-8') as file:
+        file_text = file.read()
+
+    file_surf = input_font.render(file_text, True, ('White'))
+    text_, input_rect, color_active, color_passive = setup_users_inter(font, input_font)
+
+    active = False
+    time_start = None
+    users_time = None
+    symbols_enter = 0
+    time_start = time.time()
+    while True:
+        window.blit(background, (0, 0))
+        window.blit(file_surf, (50, 100))
+        window.blit(text_, (150, 10))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_rect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+            elif event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_text = user_text[:-1]
+                        symbols_enter -= 1
+                    elif event.key == pygame.K_RETURN:
+                        if time_start is not None:
+                            total_time = time.time() - time_start
+                            typing_speed = symbols_enter / total_time
+                            time_start = None
+                            users_time = input_font.render(f"Время: {round(total_time, 2)} секунд", True,
+                                                           (255, 255, 255))
+                            characters_minute = input_font.render(
+                                f"Скорость печати: {round(typing_speed, 2)} знаков в минуту", True,
+                                pygame.Color('White'))
+                    else:
+                        user_text += event.unicode
+                        symbols_enter += 1
+
+        if active:
+            color = color_active
+        else:
+            color = color_passive
+
+        pygame.draw.rect(window, color, input_rect, 2)
+        text_surf = input_font.render(user_text, True, (255, 255, 255))
+        window.blit(text_surf, (input_rect.x + 5, input_rect.y + 5))
+        pygame.display.set_caption("Скорость печати")
+
+        input_rect.w = max(10, text_surf.get_width() + 10)
+
+        if users_time is not None:
+            window.blit(users_time, (10, 300))
+            window.blit(characters_minute, (10, 350))
+
+        pygame.display.flip()
+        clock.tick(FRAME_RATE)
+
+def main_menu():
+    run = True
+    while run:
+        window.fill((0, 0, 0))
+
+        font = pygame.font.Font(None, 72)
+        text_surf = font.render("ТЕСТ НА СКОРОСТЬ ПЕЧАТИ", True, ('Yellow'))
+        text_rect = text_surf.get_rect(center=(380, 50))
+        window.blit(text_surf, text_rect)
+
+        start_button.draw(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if start_button.rect.collidepoint(event.pos):
+                    start_test()
+
+        start_button.check_hover(pygame.mouse.get_pos())
+        pygame.display.flip()
+
+
+main_menu()
+
--- SpeedTypingTest/speedtyping/main.py
+++ SpeedTypingTest/speedtyping/main.py
@@ -1,104 +1,41 @@
+
 import pygame
 import sys
 import random
 import time
+class Button:
+    def __init__(self, x, y, width, height, text, image_path, hover_image_path=None, sound_path=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.hover_image = self.image
+        if hover_image_path:
+            self.hover_image = pygame.image.load(hover_image_path)
+            self.hover_image = pygame.transform.scale(self.hover_image, (width, height))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.sound = None
+        if sound_path:
+            self.sound = pygame.mixer.Sound(sound_path)
+        self.is_hovered = False
 
-window = pygame.display.set_mode([750, 500])
+    def draw(self, screen):
+        current_image = self.hover_image if self.is_hovered else self.image
+        screen.blit(current_image, self.rect.topleft)
 
-def main():
-    pygame.init()
-    icon = pygame.image.load("image/icon1.png")
-    pygame.display.set_icon(icon)
-    background = pygame.image.load("image/background.jpg")
-    pygame.display.set_caption("Скорость печати")
-    font = pygame.font.Font(None, 60)
-    Input_font = pygame.font.Font(None, 36)
-    clock = pygame.time.Clock()
-    user_text = ""
+        font = pygame.font.Font(None, 36)
+        text_surf = font.render(self.text,True, (255, 255, 255))
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        screen.blit(text_surf, text_rect)
 
+    def check_hover(self, mouse_pos):
+        self.is_hovered = self.rect.collidepoint(mouse_pos)
 
-    text_files = ["text1.txt", "text2.txt", "text3.txt", "text4.txt"]
-    # Выбор случайного файла из списка
-    selected_file = random.choice(text_files)
-
-    # Открытие текстового файла
-    with open(selected_file, "r", encoding='utf-8') as file:
-        file_text = file.read()
-
-    text_ = font.render("Тест на скорость печати", True, "Yellow")
-    file_surf = Input_font.render(file_text, True, ('White'))
-
-    input_rect = pygame.Rect(50, 200, 140, 32)
-    color_active = pygame.Color('Yellow')
-    color_passive = pygame.Color('White')
-    color = color_passive
-    active = False
-    time_start = None
-    users_time = None
-    symbols_enter = 0
-    #начала подсчета времени
-    time_start = time.time()
-
-    while True:
-        window.blit(background, (0, 0))
-        window.blit(file_surf, (50, 100))
-
-        window.blit(text_, (150, 10))
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if input_rect.collidepoint(event.pos):
-                    active = True
-                else:
-                    active = False
-            elif event.type == pygame.KEYDOWN:
-                if active == True:
-                    if event.key == pygame.K_BACKSPACE:
-                        user_text = user_text[:-1]
-                        symbols_enter -= 1
-                    elif event.key == pygame.K_RETURN:
-                        if time_start is not None:
-                            #расчет времени
-                            total_time = time.time() - time_start
-                            
-
-                            typing_speed = symbols_enter / total_time
-
-                            time_start = None
-                            users_time = Input_font.render(f"Время: {round(total_time, 2)} секунд", True, (255, 255, 255))
-
-                            characters_minute = Input_font.render(f"Скорость печати: {round(typing_speed, 2)} знаков в минуту", True, pygame.Color('White'))
-
-                    else:
-
-                        user_text += event.unicode
-                        symbols_enter += 1
-        if active:
-            color = color_active
-        else:
-            color = color_passive
-
-        pygame.draw.rect(window, color, input_rect, 2)
-        text_surf = Input_font.render(user_text, True, (255, 255, 255))
-        window.blit(text_surf, (input_rect.x + 5, input_rect.y + 5))
-        pygame.display.set_caption("Скорость печати")
-
-
-        input_rect.w = max(10, text_surf.get_width() + 10)
-
-        if users_time is not None:
-            window.blit(users_time, (10, 300))
-            window.blit(characters_minute, (10, 350))
-
-
-        pygame.display.flip()
-        clock.tick(60)
-
-
-if __name__ == '__main__':
-    main()
-    pygame.quit()
-
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
+            if self.sound:
+                self.sound.play()
+            pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
